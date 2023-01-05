e3e995f5e624eaff3616e044b2f3ed58764c6111
STEN <sten.li@foxmail.com>
2023-01-05 12:03:15 +0800

    glitch effect

--- src/components/TheLain.vue
+++ src/components/TheLain.vue
@@ -1,10 +1,12 @@
 <template>
-  <div v-resize="onResize" class="avatar" :style="avatarContainerStyle">
+  <div v-resize="onResize" class="avatar" :class="{ glitch }" :style="s">
     <v-img
-      :src="require('@/assets/280544@1563729391.jpg')"
-      :lazy-src="require('@/assets/280544@1563729391-lazy.jpg')"
+      :src="src"
+      :lazy-src="srcLazy"
       :height="'100%'"
       :style="avatarStyle"
+      @click="onClick"
+      @load="onLoad"
     />
   </div>
 </template>
@@ -13,22 +15,51 @@
 export default {
   name: 'LainIwakura',
   data: () => ({
+    src: require('@/assets/280544@1563729391.jpg'),
+    srcLazy: require('@/assets/280544@1563729391-lazy.jpg'),
+    loaded: false,
+    glitch: false,
     delta: 200,
   }),
   computed: {
-    avatarContainerStyle() {
-      return `
+    s() {
+      let s = `
+        position: relative;
+        z-index: 0;
         width: ${this.delta}px;
         height: ${this.delta}px;
         border-radius: 50%;
         overflow: hidden;
       `;
+      if (this.loaded) {
+        s += `
+          --src: url('${this.src}');
+        `;
+      }
+      return s;
     },
     avatarStyle() {
-      return this.avatarContainerStyle;
+      return this.s;
     },
   },
   methods: {
+    onLoad() {
+      setTimeout(() => {
+        this.loaded = true;
+        this.$nextTick(() => {
+          this.onClick();
+        });
+      }, 700);
+    },
+    onClick() {
+      if (this.glitch) {
+        return;
+      }
+      this.glitch = true;
+      setTimeout(() => {
+        this.glitch = false;
+      }, 300);
+    },
     onResize() {
       const pa = 12 * 2;
       const vw = window.innerWidth;
@@ -45,3 +76,76 @@ export default {
   },
 };
 </script>
+
+<style>
+.avatar::after {
+  content: '';
+  display: none;
+  position: absolute;
+  inset: 0;
+  z-index: 1;
+  background-image: var(--src);
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  border-radius: 50%;
+  opacity: 0;
+  visibility: hidden;
+  pointer-events: none;
+}
+.avatar.glitch::after {
+  display: block;
+  animation-name: glitch;
+  animation-duration: 300ms;
+  animation-timing-function: steps(1, end);
+}
+
+@keyframes glitch {
+  0% {
+    opacity: 1;
+    visibility: visible;
+    clip-path: inset(15% 20% 55% 33%);
+    transform: translate(-2px, -3px);
+  }
+  10% {
+    clip-path: inset(27% 10% 60% 10%);
+    transform: translate(6px, 3px);
+  }
+  20% {
+    clip-path: inset(55% 5% 33% 5%);
+    transform: translate(-3px, 9px);
+  }
+  30% {
+    clip-path: inset(71% 5% 15% 5%);
+    transform: translate(1px, 7px);
+  }
+  40% {
+    clip-path: inset(60% 10% 27% 10%);
+    transform: translate(-6px, -3px);
+  }
+  50% {
+    clip-path: inset(43% 5% 53% 5%);
+    transform: translate(-3px, 9px);
+  }
+  60% {
+    clip-path: inset(15% 20% 55% 33%);
+    transform: translate(-2px, -3px);
+  }
+  70% {
+    clip-path: inset(47% 5% 50% 5%);
+    transform: translate(-6px, -9px);
+  }
+  80% {
+    clip-path: inset(47% 5% 50% 5%);
+    transform: translate(-6px, -9px);
+  }
+  90% {
+    clip-path: inset(55% 5% 42% 5%);
+    transform: translate(7px, 1px);
+  }
+  100% {
+    clip-path: inset(15% 20% 55% 33%);
+    transform: translate(-2px, -3px);
+  }
+}
+</style>
