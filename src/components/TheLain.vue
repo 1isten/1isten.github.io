8f0d2e82f4e6d52b17d4061f0362b59954f4643e
STEN <sten.li@foxmail.com>
2023-01-06 03:12:47 +0800

    random glitch

--- src/components/TheLain.vue
+++ src/components/TheLain.vue
@@ -17,9 +17,10 @@ export default {
   data: () => ({
     src: require('@/assets/280544@1563729391.jpg'),
     srcLazy: require('@/assets/280544@1563729391-lazy.jpg'),
+    delta: 200,
     loaded: false,
     glitch: false,
-    delta: 200,
+    glitches: [],
   }),
   computed: {
     s() {
@@ -32,17 +33,44 @@ export default {
         overflow: hidden;
       `;
       if (this.loaded) {
-        s += `
-          --src: url('${this.src}');
-        `;
+        s += `--src: url('${this.src}');`;
+      }
+      if (this.glitch) {
+        s += this.g;
       }
       return s;
+    },
+    g() {
+      return this.glitches.reduce((acc, cur, i) => {
+        return (acc += `--clip-${i}:${cur.clip};--offset-${i}:${cur.offset};`);
+      }, '');
     },
     avatarStyle() {
       return this.s;
     },
   },
   methods: {
+    glitchGen() {
+      const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+      const glitches = [];
+      for (let i = 0; i < 11; i++) {
+        const h = rand(1, 10);
+        const clipInsetTop = rand(0, 100 - h);
+        const clipInsetBottom = 100 - clipInsetTop - h;
+        const clipInsetLeft = rand(0, 22);
+        const clipInsetRight = rand(0, 33);
+        const offsets = [2, 4, 8, 10];
+        const offset0 = offsets[rand(0, offsets.length - 1)];
+        const offset1 = offsets[rand(0, offsets.length - 1)];
+        const offsetX = Math.max(1, rand(-1 * offset0, offset0));
+        const offsetY = Math.max(1, rand(-1 * offset1, offset1));
+        glitches.push({
+          clip: `inset(${clipInsetTop}% ${clipInsetRight}% ${clipInsetBottom}% ${clipInsetLeft}%)`,
+          offset: `translate(${offsetX}px, ${offsetY}px)`,
+        });
+      }
+      return glitches;
+    },
     onLoad() {
       setTimeout(() => {
         this.loaded = true;
@@ -55,10 +83,13 @@ export default {
       if (this.glitch) {
         return;
       }
-      this.glitch = true;
-      setTimeout(() => {
-        this.glitch = false;
-      }, 300);
+      this.glitches = this.glitchGen();
+      this.$nextTick(() => {
+        this.glitch = true;
+        setTimeout(() => {
+          this.glitch = false;
+        }, 300);
+      });
     },
     onResize() {
       const pa = 12 * 2;
@@ -104,48 +135,70 @@ export default {
   0% {
     opacity: 1;
     visibility: visible;
-    clip-path: inset(15% 20% 55% 33%);
-    transform: translate(-2px, -3px);
+    /* clip-path: inset(15% 20% 55% 33%); */
+    /* transform: translate(-2px, -3px); */
+    clip-path: var(--clip-0);
+    transform: var(--offset-0);
   }
   10% {
-    clip-path: inset(27% 10% 60% 10%);
-    transform: translate(6px, 3px);
+    /* clip-path: inset(27% 10% 60% 10%); */
+    /* transform: translate(6px, 3px); */
+    clip-path: var(--clip-1);
+    transform: var(--offset-1);
   }
   20% {
-    clip-path: inset(55% 5% 33% 5%);
-    transform: translate(-3px, 9px);
+    /* clip-path: inset(55% 5% 33% 5%); */
+    /* transform: translate(-3px, 9px); */
+    clip-path: var(--clip-2);
+    transform: var(--offset-2);
   }
   30% {
-    clip-path: inset(71% 5% 15% 5%);
-    transform: translate(1px, 7px);
+    /* clip-path: inset(71% 5% 15% 5%); */
+    /* transform: translate(1px, 7px); */
+    clip-path: var(--clip-3);
+    transform: var(--offset-3);
   }
   40% {
-    clip-path: inset(60% 10% 27% 10%);
-    transform: translate(-6px, -3px);
+    /* clip-path: inset(60% 10% 27% 10%); */
+    /* transform: translate(-6px, -3px); */
+    clip-path: var(--clip-4);
+    transform: var(--offset-4);
   }
   50% {
-    clip-path: inset(43% 5% 53% 5%);
-    transform: translate(-3px, 9px);
+    /* clip-path: inset(43% 5% 53% 5%); */
+    /* transform: translate(-3px, 9px); */
+    clip-path: var(--clip-5);
+    transform: var(--offset-5);
   }
   60% {
-    clip-path: inset(15% 20% 55% 33%);
-    transform: translate(-2px, -3px);
+    /* clip-path: inset(15% 20% 55% 33%); */
+    /* transform: translate(-2px, -3px); */
+    clip-path: var(--clip-6);
+    transform: var(--offset-6);
   }
   70% {
-    clip-path: inset(47% 5% 50% 5%);
-    transform: translate(-6px, -9px);
+    /* clip-path: inset(47% 5% 50% 5%); */
+    /* transform: translate(-6px, -9px); */
+    clip-path: var(--clip-7);
+    transform: var(--offset-7);
   }
   80% {
-    clip-path: inset(47% 5% 50% 5%);
-    transform: translate(-6px, -9px);
+    /* clip-path: inset(47% 5% 50% 5%); */
+    /* transform: translate(-6px, -9px); */
+    clip-path: var(--clip-8);
+    transform: var(--offset-8);
   }
   90% {
-    clip-path: inset(55% 5% 42% 5%);
-    transform: translate(7px, 1px);
+    /* clip-path: inset(55% 5% 42% 5%); */
+    /* transform: translate(7px, 1px); */
+    clip-path: var(--clip-9);
+    transform: var(--offset-9);
   }
   100% {
-    clip-path: inset(15% 20% 55% 33%);
-    transform: translate(-2px, -3px);
+    /* clip-path: inset(15% 20% 55% 33%); */
+    /* transform: translate(-2px, -3px); */
+    clip-path: var(--clip-10);
+    transform: var(--offset-10);
   }
 }
 </style>
